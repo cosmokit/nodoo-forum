e5a2d976bba1fe6ea2a3ac3481af20f5d3b1ba0d
Alex Chesnay <me@alex-chesnay.fr>
2019-10-31 23:33:34 +0100

    frontend: React & Sass

---
+++ assets/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <h1>Nodoo Forum</h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+import "./scss/main.scss";
